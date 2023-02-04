e6690d988735c75ee4e29c58f35357c55da513e9
farha <farha@FARHAD>
2023-02-05 00:32:32 +0400

    Proje dosyası ekle.

---
+++ reqemler/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace reqemler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 2
+            //Console.WriteLine("Reqem daxil ele ");
+            //int reqem = int.Parse(Console.ReadLine());
+            //int r;
+            //int log = 0;
+            
+            //int hasil = 0;
+            //int final = 0;
+            
+
+
+            //while (reqem != 0)
+            //{
+            //    r = reqem % 10;
+            //    reqem /= 10;
+            //    log++;
+           
+            //    hasil = log*r;
+            //    final += hasil;
+
+                
+
+            //}
+            //Console.WriteLine(final);
+            #endregion
+
+
+          
+            Console.WriteLine("Reqem daxil ele ");
+            int reqem = int.Parse(Console.ReadLine());
+            int r;
+            int log = 0;
+            int kvadrat;
+
+            
+
+
+
+            while (reqem != 0)
+            {
+                r = reqem % 10;
+                reqem /= 10;
+             
+                log++;
+              
+
+                kvadrat =  r*r;
+               
+
+                Console.WriteLine(kvadrat);
+
+
+
+
+
+            }
+            
+        }
+    }
+}
